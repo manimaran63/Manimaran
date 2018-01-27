45954074febb577b23a75b807e59fe84b60c8868
manimaran63 <35652845+manimaran63@users.noreply.github.com>
2018-01-27 19:26:55 +0530

    Create Vowel.c

---
+++ Vowel.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   char ch;
+   clrscr();
+   printf("Enter the letter");
+   scanf("%c",&c);
+   if(ch=='a'!!ch=='e'!!ch=='i'!!ch=='o'!!ch=='u')
+   {
+      printf("Vowel");
+   }
+   else
+   {
+      Printf("Consonant");
+   }
+   getch();
+ }
