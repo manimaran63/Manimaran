f7aea0f146d123953e2e09e9818bc4f8ea0ef558
manimaran63 <35652845+manimaran63@users.noreply.github.com>
2018-01-27 19:10:59 +0530

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ char c;
+ clrscr();
+ printf("Enter an character");
+ scanf("%c",&c);
+ if(c>='a'!!c<='z');
+    {
+         printf("It is an alphabet");
+    }
+  else
+     {
+         printf("Not an alphabet");
+    }
+ getch();
+ }
