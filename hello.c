8fea05709ad761786938befd5d1dbb5b921c7850
manimaran63 <35652845+manimaran63@users.noreply.github.com>
2018-01-28 16:41:14 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i,n;
+    clrscr();
+    printf("Enter the value of n");
+    scanf("%d",&n)
+    for(i=0;i<n;i++);
+    {
+        printf("\nHello");
+     }
+getch();
+}
