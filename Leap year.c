cdb8a2eb6ae32f5ca31a7d6269d0d588241de3fc
manimaran63 <35652845+manimaran63@users.noreply.github.com>
2018-01-28 12:03:08 +0530

    Create Leap year.c

---
+++ Leap year.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   int year;
+   clrscr();
+   printf("Enter the year");
+   scanf("%d",&year);
+   if(a%4==0)
+   {
+      printf("It is leap year");
+   }
+   else
+   {
+        printf("It is not leap year");
+   }
+   getch();
+ }
