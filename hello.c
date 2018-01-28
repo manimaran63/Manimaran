1b8ff0cdde813816aeb9de153579ac19774bcda1
manimaran63 <35652845+manimaran63@users.noreply.github.com>
2018-01-28 16:44:04 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -5,8 +5,8 @@ void main()
     int i,n;
     clrscr();
     printf("Enter the value of n");
-    scanf("%d",&n)
-    for(i=0;i<n;i++);
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
     {
         printf("\nHello");
      }
