670ced9e9caf3390e25261b52183944ed63044f4
manimaran63 <35652845+manimaran63@users.noreply.github.com>
2018-01-28 16:58:15 +0530

    Create natural.c

---
+++ natural.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   int i,n,sum=0;
+   clrscr();
+   printf("Enter a positive number");
+   scanf("%d",&n);
+   for(i=0;i<=n;i++)
+   {
+      Printf("sum is %d",sum);
+      sum=sum+i;
+   }
+getch();
+}   
