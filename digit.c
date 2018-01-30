b451f373205f639147ddd9756e16a1a2af6ecf5d
manimaran63 <35652845+manimaran63@users.noreply.github.com>
2018-01-30 16:29:49 +0530

    Create digit.c

---
+++ digit.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  long int n;
+  int b;
+  clrscr();
+  printf("Enter the integer");
+  scanf("%ld",&a);
+  while(n!=0)
+  {
+    n\=10;
+    ++b;
+  }
+   printf("The sum of integer is %d",b);
+   getch();
+   }
