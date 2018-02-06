2e86b14364a6eb3a2df8be66c99a8994eaaf74db
manimaran63 <35652845+manimaran63@users.noreply.github.com>
2018-02-06 16:21:54 +0530

    Create No16.c

---
+++ No16.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a,b,i,flag;
+ clrscr();
+ printf("Enter the two intervals:");
+ scanf("%d%d",&a,&b);
+ while(a<b)
+ {
+    flag=0;
+    if(i=2;i<=a/2;++1)
+    {
+      if(a%i==0)
+      {
+       flag=1;
+       break;
+    }
+ }
+ if(flag==0)
+ {
+   printf("%d",a);
+ Com    ++a;
+ } 
+}
+getch();
+}
