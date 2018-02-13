903e921528ccfc9fd413ccca3b7c1bd16f72d5f5
manimaran63 <35652845+manimaran63@users.noreply.github.com>
2018-02-13 16:48:56 +0530

    Create No38.c

---
+++ No38.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a=10,b=15;
+ clrscr();
+ printf("Before swap");
+ printf("%d\t%d",a,b);
+ a=a^b,b=b^a,a=a^b;
+ printf("\nAfter swap");
+ printf("%d\t%d",a,b);
+ getch();
+} 
