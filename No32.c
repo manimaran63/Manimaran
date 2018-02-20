f36bef94f5ea1fba0f094cc4c769b19dfbf76ecc
manimaran63 <35652845+manimaran63@users.noreply.github.com>
2018-02-20 16:25:34 +0530

    Create No32.c

---
+++ No32.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+  char a[50];
+  int i,count=0;
+  clrscr();
+  printf("Enter the string\n");
+  scanf("%[^\n]s",&a[i]);
+  for(i=0;s[i]!='\n';i++)
+  {
+     if(a[i]==' ')
+     count++;
+  }
+  printf("number of words in the line is %d",count+1);
+  getch();
+}  
