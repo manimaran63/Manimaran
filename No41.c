adad8f3e9a028a96b530c35fa0dbf435afb09a96
manimaran63 <35652845+manimaran63@users.noreply.github.com>
2018-02-22 16:55:23 +0530

    Create No41.c

---
+++ No41.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+  int i,n;
+  char a[50];
+  clrscr();
+  printf("Enter n value");
+  scan("%d",&n);
+  for(i=0;i<n;i++)
+  { 
+    printf("\nManimaran");
+  }
+ getch();
+} 
