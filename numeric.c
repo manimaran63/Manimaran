739f95999f054ce6ffdc5fa9f622d9f7e4d87e6d
manimaran63 <35652845+manimaran63@users.noreply.github.com>
2018-02-22 16:43:41 +0530

    Create numeric.c

---
+++ numeric.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+  int i,count=0;
+  char a[i];
+  clrscr();
+  printf("Enter the string");
+  scanf("%s",&a[i];
+  for(i=0;a[i]!='/0';i++)
+  {
+   if(a[i]>='0'&&a[i]<='9')
+   {
+     count=count+1;
+   }
+   printf("\nNumber of numeric elements are%d",count);
+  }
+getch();
+}
