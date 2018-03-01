cbcc6f12093e3272fed9fac5409bc4e13a882d8b
manimaran63 <35652845+manimaran63@users.noreply.github.com>
2018-03-01 15:45:52 +0530

    Create No49.c

---
+++ No49.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a,b,c;
+  clrscr();
+  printf("Enter the values of a and b");
+  scanf("%d\t%d",&a,&b);
+  c=a+b;
+  printf("The sum is %d",c);
+  getch();
+}  
