51b3433f61074decdb6a1f3b6d0da0342ebc1610
manimaran63 <35652845+manimaran63@users.noreply.github.com>
2018-03-02 15:00:08 +0530

    Create NO47.c

---
+++ NO47.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+void main() 
+{
+int a[100];
+int i,n;
+clrscr();
+printf("Enter n value");
+scanf("%d",&n);
+printf("Enter the elements");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+int max=a[0];
+int min=a[0];
+for(i=1;i<n;i++)
+{
+if(a[i]>max)
+{
+max=a[i];
+}
+if(a[i]<min)
+{
+min=a[i];
+}
+}
+printf("\n%d",min);
+printf("\n%d",max);
+getch();
+}
