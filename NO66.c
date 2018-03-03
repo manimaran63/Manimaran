230ec241c30d232951647aa87a8f784cbbe458e5
manimaran63 <35652845+manimaran63@users.noreply.github.com>
2018-03-03 16:48:54 +0530

    Create NO66.c

---
+++ NO66.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int n,i,s=0;
+  clrscr();
+  printf("Enter a positive integer");
+  scanf("%d",&n);
+  for(i=2;i<=n/2;++i)
+  {
+      if(n%i==0)
+      {
+           s=1;
+           break;
+      }
+  }
+  if(s==0)
+  {
+      printf("prime number");
+  }
+ else
+ {
+    printf("Not a prime number");
+ }
+getch();
+}
