d2b3db8e64e9105435f298bbf89d1a5b6b26c8dd
manimaran63 <35652845+manimaran63@users.noreply.github.com>
2018-03-03 16:29:07 +0530

    Create No55.c

---
+++ No55.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int m=12,n=3,a;
+    clrscr();
+    a=m*n;
+    if(a%2==0)
+      printf("Even numbr");
+    else
+      printf("Odd number");
+    getch();
+}    
+   
