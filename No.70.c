ce97c5982847009a4dce02f20fbed1967446a09d
manimaran63 <35652845+manimaran63@users.noreply.github.com>
2018-03-15 16:01:50 +0530

    Create No.70.c

---
+++ No.70.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b;
+    clrscr();
+    printf("Enter the value of a");
+    scanf("%d",&a);
+    b=a*2;
+    printf("%d",b);
+    getch();
+}
