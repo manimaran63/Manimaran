03779fb0fb1fd9c03415ae0ac8aeea79b70edbc1
manimaran63 <35652845+manimaran63@users.noreply.github.com>
2018-03-16 14:39:17 +0530

    Create No.80.c

---
+++ No.80.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,i,r;
+  printf("\nenter a number:\n");
+  scanf("%d",&n);
+  while(n!=0)
+  {
+    r=n%10;
+    if(r%2!=0)
+    {
+      printf("\n%d",r);
+    }
+    n/=10;
+  }
+}
