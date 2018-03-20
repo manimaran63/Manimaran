a53ca4d74b766b0d46fb240d1591e196c865a690
manimaran63 <35652845+manimaran63@users.noreply.github.com>
2018-03-20 16:15:31 +0530

    Create No.73.c

---
+++ No.73.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a=2,b=6,c=3;
+    clrscr();
+    if((c>=a)&&(c<=b))
+    {
+        printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+    getch();
+}    
