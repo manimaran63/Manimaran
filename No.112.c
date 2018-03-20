8c5733a3da34442171d9d61a9d4419f647c3f30d
manimaran63 <35652845+manimaran63@users.noreply.github.com>
2018-03-20 16:54:13 +0530

    Create No.112.c

---
+++ No.112.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,a[10],i,b;
+    clrscr();
+    printf("Enter n value");
+    scanf("%d",n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("Enter c value");
+    scanf("%d",c);
+    if(a[i]>=c<=a[i])
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("No");
+    }
+    getch();
+} 
