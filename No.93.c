65a9ade4331d1037f403de189376edfc631e0bec
manimaran63 <35652845+manimaran63@users.noreply.github.com>
2018-03-20 16:30:04 +0530

    Create No.93.c

---
+++ No.93.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b;
+    clrscr();
+    printf("Enter the temperature");
+    scanf("%d",&a);
+    b=a+273;
+    printf("%d",b);
+    getch();
+}    
