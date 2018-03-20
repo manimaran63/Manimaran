56b9137f847216ba1c55902cc8c89965ea830c56
manimaran63 <35652845+manimaran63@users.noreply.github.com>
2018-03-20 16:10:29 +0530

    Create No.74.c

---
+++ No.74.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    float b;
+    int a;
+    clrscr();
+    printf("Enter the number");
+    scanf("%f",&b);
+    a=(b+0.5);
+    printf("%d",a);
+    getch();
+}    
