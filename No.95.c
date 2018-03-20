c181246c9d47303419620a3ccf4e048c86129353
manimaran63 <35652845+manimaran63@users.noreply.github.com>
2018-03-20 16:35:09 +0530

    Create No.95.c

---
+++ No.95.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int p,n,r,i;
+    clrscr();
+    printf("Enter the values p & n & r");
+    scanf("%d\t%d\t%d",&p,&n,&r);
+    i=(p*n*r)/100;
+    printf("%d",i);
+    getch();
+}    
