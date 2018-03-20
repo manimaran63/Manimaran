6eb49ab0a1834106ea7ba92da1823bcab4640259
manimaran63 <35652845+manimaran63@users.noreply.github.com>
2018-03-20 16:02:25 +0530

    Create No.104.c

---
+++ No.104.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,c;
+    clrscr();
+    printf("Enter a and b");
+    scanf("%d%d",&a,&b);
+    c=pow(a,b);
+    printf("%d",c);
+    getch();
+}    
